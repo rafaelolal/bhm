4211c6b57eb0589d7a4c993536af0c15b8d2ddf7
Rafael Almeida <rafaelpbcp@gmail.com>
2023-03-20 14:10:18 -0400

    added link tag to star component

--- components/star.tsx
+++ components/star.tsx
@@ -1,14 +1,17 @@
 import Image from 'next/image'
+import Link from 'next/link'
 
 export default function Star(props: { lastName: string }) {
   return (
     <>
-      <Image
-        src={`/images/people/${props.lastName}.jpg`}
-        width={300}
-        height={300}
-        alt={props.lastName}
-      />
+      <Link href={`https:/ralmeida.dev/bhm/${props.lastName}`}>
+        <Image
+          src={`/images/people/${props.lastName}.jpg`}
+          width={300}
+          height={300}
+          alt={props.lastName}
+        />
+      </Link>
     </>
   )
 }
