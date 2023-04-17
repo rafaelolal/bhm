c7d8df8ed5056fc0f70783fc378f63bfeba188ba
Rafael Almeida <rafaelpbcp@gmail.com>
2023-04-17 19:33:43 -0400

    proper favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html lang="en" style={{ scrollBehavior: 'smooth' }}>
       <Head />
+      <link rel='icon' href='/bhm/favicon.ico' />
       <body className="backdrop">
         <Main />
         <NextScript />
